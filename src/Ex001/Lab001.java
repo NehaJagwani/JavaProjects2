adf3537d70fe340ae6096b445bc7376cd4187322
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 12:38:33 +1000

    added 1 more line

--- src/Ex001/Lab001.java
+++ src/Ex001/Lab001.java
@@ -3,5 +3,6 @@ package Ex001;
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("learing to push the code to GitHub");
+        System.out.println("wonderful job Neha");
     }
 }
