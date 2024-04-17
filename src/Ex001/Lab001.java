69adec07c7b8c58ffcb712fc402db028891c7e44
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 13:53:05 +1000

    Removed file

--- src/Ex001/Lab001.java
+++
@@ -1,8 +0,0 @@
-package Ex001;
-
-public class Lab001 {
-    public static void main(String[] args) {
-        System.out.println("learing to push the code to GitHub");
-        System.out.println("wonderful job Neha");
-    }
-}
